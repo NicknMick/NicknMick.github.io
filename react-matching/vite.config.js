b75abde09d3ae21780cbc5c39b4604b9df6585dc
nicknmick <nickschwam@gmail.com>
2025-02-18 18:31:40 -0800

    Fixed index html

--- react-matching/vite.config.js
+++ react-matching/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/NicknMick.github.io",
   plugins: [react()],
+  base: "/NicknMick.github.io/react-matching/"
 })
