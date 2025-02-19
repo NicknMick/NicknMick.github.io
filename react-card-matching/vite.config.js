b7cea5561810c4e65d41e5245db9fe04cf7f102b
nicknmick <nickschwam@gmail.com>
2025-02-18 21:47:30 -0800

    Fixed index html

--- react-card-matching/vite.config.js
+++ react-card-matching/vite.config.js
@@ -3,6 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-card-matching/",
   plugins: [react()],
-  base: "/react-card-matching/"
-})
+
+  esbuild: {
+    loader: 'jsx',
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+});
